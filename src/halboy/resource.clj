6ce5f1e5e7c15edf9c9a0013679ef90c00bab041
Jimmy Thompson <jimmy.thompson@thoughtworks.com>
2017-07-05 22:43:07 +0100

    Add resource creation functions

--- src/halboy/resource.clj
+++ src/halboy/resource.clj
@@ -2,16 +2,19 @@
   (:require [clojure.string :as str]
             [halboy.params :as params]))
 
+(defn- create-or-append [previous current]
+  (cond
+    (nil? previous) current
+    (list? previous) (conj previous current)
+    :else (list previous current)))
+
 (defrecord Resource [links embedded properties])
 
-(defn new-resource [links embedded properties]
-  (->Resource links embedded properties))
-
-(defn new-embedded-resource [links properties]
-  (->Resource links {} properties))
-
-(defn new-index-resource [links]
-  (->Resource links {} {}))
+(defn new-resource
+  ([]
+   (->Resource {} {} {}))
+  ([links embedded properties]
+   (->Resource links embedded properties)))
 
 (defn get-link
   ([resource rel]
@@ -25,3 +28,29 @@
 
 (defn get-property [resource key]
   (get-in resource [:properties key]))
+
+
+(defn add-link [resource rel m]
+  (let [existing-links (:links resource)
+        updated-link (-> (get existing-links rel)
+                         (create-or-append m))]
+    (->Resource
+      (assoc existing-links rel updated-link)
+      (:embedded resource)
+      (:properties resource))))
+
+(defn add-resource [resource rel r]
+  (let [existing-resources (:embedded resource)
+        updated-resource (-> (get existing-resources rel)
+                             (create-or-append r))]
+    (->Resource
+      (:links resource)
+      (assoc existing-resources rel updated-resource)
+      (:properties resource))))
+
+(defn add-property [resource rel r]
+  (let [existing-properties (:properties resource)]
+    (->Resource
+      (:links resource)
+      (:embedded resource)
+      (assoc existing-properties rel r))))
--- test/halboy/resource_test.clj
+++ test/halboy/resource_test.clj
@@ -1,67 +1,75 @@
 (ns halboy.resource-test
   (:require
     [expectations :refer :all]
-    [halresource.resource
+    [halboy.resource :as halboy]
+    [halboy.resource
      :refer [new-resource
-             add-link]]
-    [halboy.resource :as halboy]))
+             add-link
+             add-resource
+             add-property
+             get-link
+             get-embedded
+             get-property]]))
+
+(defn- new-embedded-resource [links properties]
+  (new-resource links {} properties))
 
 (let [resource
-      (halboy/new-resource
-        {:self     {:href "/orders"},
-         :curies   [{:name      "ea",
-                     :href      "http://example.com/docs/rels/{rel}",
-                     :templated true}],
-         :next     {:href "/orders?page=2"},
-         :ea:find  {:href      "/orders{?id}",
-                    :templated true},
-         :ea:admin [{:href "/admins/2", :title "Fred"}
-                    {:href "/admins/5", :title "Kate"}]}
-        {:ea:order [(halboy/new-embedded-resource
-                      {:self        {:href "/orders/123"},
-                       :ea:basket   {:href "/baskets/98712"},
-                       :ea:customer {:href "/customers/7809"}}
-                      {:total    30.0,
-                       :currency "USD",
-                       :status   "shipped"})
-                    (halboy/new-embedded-resource
-                      {:self        {:href "/orders/124"},
-                       :ea:basket   {:href "/baskets/97213"},
-                       :ea:customer {:href "/customers/12369"}}
-                      {:total    20.0,
-                       :currency "USD",
-                       :status   "processing"})]}
-        {:currently-processing 14
-         :shipped-today        20})]
+      (-> (new-resource)
+          (add-link :self {:href "/orders"})
+          (add-link :curies {:name      "ea",
+                             :href      "http://example.com/docs/rels/{rel}",
+                             :templated true})
+          (add-link :next {:href "/orders?page=2"})
+          (add-link :ea:find {:href      "/orders{?id}",
+                              :templated true})
+          (add-link :ea:admin [{:href "/admins/2", :title "Fred"}
+                               {:href "/admins/5", :title "Kate"}])
+          (add-resource :ea:order (new-embedded-resource
+                                    {:self        {:href "/orders/123"},
+                                     :ea:basket   {:href "/baskets/98712"},
+                                     :ea:customer {:href "/customers/7809"}}
+                                    {:total    30.0,
+                                     :currency "USD",
+                                     :status   "shipped"}))
+          (add-resource :ea:order (new-embedded-resource
+                                    {:self        {:href "/orders/124"},
+                                     :ea:basket   {:href "/baskets/97213"},
+                                     :ea:customer {:href "/customers/12369"}}
+                                    {:total    20.0,
+                                     :currency "USD",
+                                     :status   "processing"}))
+          (add-property :currently-processing 14)
+          (add-property :shipped-today 20))]
 
   ; get-link should get a link from a resource
   (expect
     "/orders?page=2"
-    (halboy/get-link resource :next))
+    (get-link resource :next))
 
   ; get-link should expand templated urls
   (expect
     "/orders/my-order"
-    (halboy/get-link resource :ea:find {:id "my-order"}))
+    (get-link resource :ea:find {:id "my-order"}))
 
   ; get-link should remove unresolved template parameters
   (expect
     "/orders"
-    (halboy/get-link resource :ea:find))
+    (get-link resource :ea:find))
 
   ; get-embedded should get an embedded resource
   (expect
-    (halboy/new-embedded-resource
+    (new-embedded-resource
       {:self        {:href "/orders/123"},
        :ea:basket   {:href "/baskets/98712"},
        :ea:customer {:href "/customers/7809"}}
       {:total    30.0,
        :currency "USD",
        :status   "shipped"})
-    (-> (halboy/get-embedded resource :ea:order)
+    (-> (get-embedded resource :ea:order)
         first))
 
   ; get-property should get a property from the body
   (expect
     14
-    (halboy/get-property resource :currently-processing)))
+    (get-property resource :currently-processing)))
