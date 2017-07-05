4db5b7ac57d4fb1d7cbbf09b47ae1e41a7d14cd4
Jimmy Thompson <jimmy.thompson@thoughtworks.com>
2017-07-05 23:57:23 +0100

    Split out to/from json functions

--- src/halboy/json.clj
+++ src/halboy/json.clj
@@ -1,8 +1,62 @@
 (ns halboy.json
-  (:require [cheshire.core :as json]))
+  (:use clojure.pprint)
+  (:require
+    [clojure.walk :refer [keywordize-keys]]
+    [halboy.data :refer [transform-values]]
+    [halboy.resource :refer [new-resource]]
+    [cheshire.core :as json]))
 
-(defn json->map [s]
-  (json/parse-string s))
+(defn- extract-links [m]
+  (:_links m))
 
-(defn map->json [m]
-  (json/generate-string m {:pretty true}))
+(defn- extract-properties [m]
+  (dissoc m :_links :_embedded))
+
+(defn- map->embedded-resource [m]
+  (if (map? m)
+    (new-resource
+      (extract-links m)
+      {}
+      (extract-properties m))
+    (map map->embedded-resource m)))
+
+(defn- extract-embedded [body]
+  (-> (get body :_embedded {})
+      (transform-values map->embedded-resource)))
+
+(defn- map->resource [m]
+  (new-resource
+    (extract-links m)
+    (extract-embedded m)
+    (extract-properties m)))
+
+(defn- links->map [resource]
+  (let [links (:links resource)]
+    (when (not (empty? links))
+      {:_links links})))
+
+(defn- embedded-resource->map [resource]
+  (if (map? resource)
+    (merge
+      (links->map resource)
+      (:properties resource))
+    (map embedded-resource->map resource)))
+
+(defn- embedded->map [resource]
+  (let [resources (-> (:embedded resource)
+                      (transform-values embedded-resource->map))]
+    (when (not (empty? resources))
+      {:_embedded resources})))
+
+
+(defn json->resource [s]
+  (-> (json/parse-string s)
+      keywordize-keys
+      map->resource))
+
+(defn resource->json [resource]
+  (-> (merge
+        (links->map resource)
+        (embedded->map resource)
+        (:properties resource))
+      json/generate-string))
--- src/halboy/navigator.clj
+++ src/halboy/navigator.clj
@@ -2,37 +2,15 @@
   (:require [clojure.walk :refer [keywordize-keys]]
             [halboy.resource :as resource]
             [halboy.data :refer [transform-values]]
-            [halboy.json :refer [json->map map->json]]
+            [halboy.json :refer [json->resource]]
             [halboy.http :refer [GET POST]]))
 
 (defrecord Navigator [href resource meta])
 
-(defn- extract-body [response]
-  (->
-    (:body response)
-    json->map
-    keywordize-keys))
-
-(defn- extract-links [body]
-  (:_links body))
-
-(defn- extract-embedded [body]
-  (get body :_embedded {}))
-
-(defn- extract-properties [body]
-  (dissoc body :_links :_embedded))
-
-(defn- extract-resource [resource]
-  (resource/new-resource
-    (extract-links resource)
-    (extract-embedded resource)
-    (extract-properties resource)))
-
 (defn- extract-navigator-from-response [response]
   (let [current-url (get-in response [:opts :url])
-        resource (-> response
-                     extract-body
-                     extract-resource)]
+        resource (-> (:body response)
+                     json->resource)]
     (->Navigator
       current-url
       resource
