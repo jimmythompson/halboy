e53e1dd13b80f89de2f2e9505c3fbbf42f587b5a
Jimmy Thompson <jimmy.thompson@thoughtworks.com>
2017-10-09 15:43:24 +0100

    Bump version to 2.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject halboy "2.0.2"
+(defproject halboy "2.0.3"
   :description "a hypermedia parser and navigator"
   :license {:name "MIT License"
             :url  "https://opensource.org/licenses/MIT"}
