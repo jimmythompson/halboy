82be304a559d95e25f9fd7cebad44cd237a9acd9
jimmythompson <jimmy@jimmythompson.co.uk>
2018-08-12 21:40:00 +0100

    Bump version to 4.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject halboy "3.1.2"
+(defproject halboy "4.0.0"
   :description "a hypermedia parser and navigator"
   :license {:name "MIT License"
             :url  "https://opensource.org/licenses/MIT"}
