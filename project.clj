7b7787bf90ee49be68cfe774bae8b5db9ccff4d9
Daniel Żurawski <zurawski.daniel@gmail.com>
2020-08-24 11:00:19 +0100

    feat(clj-http): new client, replicate http and navigator tests

--- project.clj
+++ project.clj
@@ -10,9 +10,11 @@
                  [medley "1.4.0"]
                  [uritemplate-clj "1.3.1"]
                  [org.clojure/core.cache "1.0.225"]
-                 [org.bovinegenius/exploding-fish "0.3.6"]]
+                 [org.bovinegenius/exploding-fish "0.3.6"]
+                 [clj-http "3.10.2"]]
   :plugins [[lein-eftest "0.5.2"]]
   :profiles {:shared {:dependencies [[nrepl "1.0.0"]]}
              :test   [:shared {:dependencies [[http-kit.fake "0.2.2"]
+                                              [clj-http-fake "1.0.3"]
                                               [eftest "0.5.2"]]}]}
   :eftest {:multithread? false})
