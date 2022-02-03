7954d5c6cd9adcd1b8200cd949e582cd7dc39f88
Alex Vear <alex.vear@kroo.com>
2022-02-03 12:18:58 +0000

    Update all dependencies
    Authors: @axvr, @sonalchandani

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :license {:name "MIT License"
             :url  "https://opensource.org/licenses/MIT"}
   :url "https://github.com/jimmythompson/halboy"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [http-kit "2.3.0"]
-                 [cheshire "5.8.0"]
-                 [medley "1.0.0"]
-                 [uritemplate-clj "1.2.1"]
-                 [org.clojure/core.cache "0.7.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [http-kit "2.5.3"]
+                 [cheshire "5.10.2"]
+                 [medley "1.3.0"]
+                 [uritemplate-clj "1.3.1"]
+                 [org.clojure/core.cache "1.0.225"]
                  [org.bovinegenius/exploding-fish "0.3.6"]]
   :plugins [[lein-eftest "0.5.9"]]
-  :profiles {:shared {:dependencies [[nrepl "0.6.0"]]}
+  :profiles {:shared {:dependencies [[nrepl "0.9.0"]]}
              :test   [:shared {:dependencies [[http-kit.fake "0.2.2"]
                                               [eftest "0.5.9"]]}]}
   :eftest {:multithread? false})
