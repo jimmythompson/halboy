21d1685f508af3f5016beffbcd86cf111aacbcdf
Dominic Freeston <dominic@freeston.me>
2023-07-18 17:01:51 +0100

    Fix a bug in deep-merge that turns false into nil

--- src/halboy/argutils.clj
+++ src/halboy/argutils.clj
@@ -34,5 +34,5 @@
             (if (and (map? v1) (map? v2))
               (merge-with deep-merge v1 v2)
               v2))]
-    (when (some identity vs)
+    (when (seq vs)
       (reduce #(rec-merge %1 %2) v vs))))
